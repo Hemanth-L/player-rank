78a1c7f21819ad1af5d89c535850df69832db95b
Hemanth-L <67827171+Hemanth-L@users.noreply.github.com>
2020-11-07 19:53:49 +0530

    Add files via upload

---
+++ js/rank.js
@@ -0,0 +1,10 @@
+class Rank{
+    constructer(){
+
+    }
+    display(){
+        var victory=createElement('h3')
+        victory.position(displayWidth/2-100,displayHeight/4)
+        victory.html("player rank: "+player.rank)
+    }
+}
